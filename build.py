1aaa91dfc7aa943d870f0938bba8edbee03428ee
Bea Young <beay4338@gmail.com>
2022-05-19 21:23:22 +0100

    Add loads of things and finish build script

--- build.py
+++ build.py
@@ -1,2 +1,33 @@
 # TODO 
 # Auto-remove comments, auto-preobfuscate, auto-test* if possible
+
+import os.path 
+from os import mkdir
+
+buildFile = open('sentiment.lua', 'r')
+buildLines = buildFile.readlines()
+
+lineCount = 0
+commentCount = 0
+
+lines = []
+
+for line in buildLines:
+    lineCount += 1
+    if(line.startswith("--")):
+        commentCount += 1
+    else:
+        lines.append(line)
+
+print("{*} Removed ", commentCount, " comments!")
+
+
+if(os.path.exists("build")):
+    print("{*} Writing to ./build/compiled.lua")
+else:
+    print("{*} Creating build folder")
+    mkdir("build")
+    print("{*} Writing to ./build/compiled.lua")
+
+compiledFile = open("build/compiled.lua", 'x')
+compiledFile.writelines(lines)
